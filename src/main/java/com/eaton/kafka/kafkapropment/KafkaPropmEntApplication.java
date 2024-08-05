679bb3b64de0e2536420f5ce17d82fabcb22c99b
Asad <asad3gh@gmail.com>
2024-08-05 00:40:21 -0400

    Initial commit

---
+++ src/main/java/com/eaton/kafka/kafkapropment/KafkaPropmEntApplication.java
@@ -0,0 +1,13 @@
+package com.eaton.kafka.kafkapropment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPropmEntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPropmEntApplication.class, args);
+    }
+
+}
